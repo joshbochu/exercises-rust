7f9f441c06b803e2ffed5bd1a07377435f66bff9
Josh Bochu <joshuatbochu+git@gmail.com>
2022-02-07 19:40:48 -0500

    cleanup rpn

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -9,57 +9,23 @@ pub enum CalculatorInput {
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     let mut vec: Vec<i32> = Vec::new();
-    let mut ret: Option<i32> = None;
-    for i in inputs.iter() {
+    for i in inputs {
         match i {
-            CalculatorInput::Value(x) => {
-                vec.push(*x)
-            },
-            CalculatorInput::Subtract => {
-                let mut num1 = vec.pop();
-                let mut num2 = vec.pop();
-                match (num1, num2) {
-                    (Some(x), Some(y)) => {
-                        vec.push(y-x);
-                        Some(0)
-                    },
-                    _ => { return None; }
-                };
-            },
-            CalculatorInput::Add => {
-                let mut num1 = vec.pop();
-                let mut num2 = vec.pop();
-                match (num1, num2) {
-                    (Some(x), Some(y)) => {
-                        vec.push(y + x);
-                        Some(0)
-                    },
-                    _ => { return None; }
-                };
-            },
-            CalculatorInput::Multiply => {
-                let mut num1 = vec.pop();
-                let mut num2 = vec.pop();
-                match (num1, num2) {
-                    (Some(x), Some(y)) => {
-                        vec.push(x*y);
-                        Some(0)
-                    },
-                    _ => { return None; }
-                };
-            },
-             CalculatorInput::Divide => {
-                let mut num1 = vec.pop();
-                let mut num2 = vec.pop();
-                match (num1, num2) {
-                    (Some(x), Some(y)) => {
-                        vec.push(y / x);
-                        Some(0)
-                    },
-                    _ => { return None; }
-                };
-            },
-            _ => { return None; }
+            CalculatorInput::Value(v) => vec.push(*v),
+            _ => match vec.len() {
+                0 | 1 => return None,
+                _ => {
+                    let a = vec.pop().unwrap();
+                    let b = vec.pop().unwrap();
+                    match i {
+                        CalculatorInput::Add => vec.push(b + a),
+                        CalculatorInput::Subtract => vec.push(b - a),
+                        CalculatorInput::Multiply => vec.push(b * a),
+                        CalculatorInput::Divide => vec.push(b / a),
+                        _ => return None
+                    }
+                }
+            }
         }
     }
     if vec.len() == 1 { vec.pop() } else { None }
